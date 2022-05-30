491e9432152a9bf03474715d6f6088d617148f2e
durvallins <durval.lsiqueira@gmail.com>
2022-05-30 14:08:51 -0300

    Update MinMax.js

--- exercicios/src/components/MinMax.js
+++ exercicios/src/components/MinMax.js
@@ -2,10 +2,21 @@ import React from 'react'
 import { Text } from 'react-native'
 import Estilo from './estilo'
 
+<<<<<<< Updated upstream
 export default props => (
       < Text style={Estilo.txtG} >
         O valor {props.max} é maior que o valor {props.min}.
       </Text >
 
     )
+=======
+export default (param) => {
+  console.warn(param)
+  return (
+  <Text style={Estilo.txtG}>
+    O valor X é maior que o valor Y.
+  </Text>
+  )
+}
+>>>>>>> Stashed changes
 
