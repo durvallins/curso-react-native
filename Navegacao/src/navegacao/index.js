5b3a75434b2bed79e39b2b80539d50a35eeb5c26
durvallins <durval.lsiqueira@gmail.com>
2022-06-15 22:10:50 -0300

    Inserção dos ícones

--- Navegacao/src/navegacao/index.js
+++ Navegacao/src/navegacao/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
-import Drawer from './Drawer';
-//import Tab from './Tab';
+
+//import Drawer from './Drawer';
+import Tab from './Tab';
 //import Stack from './Stack';
 
 export default props => (
     <SafeAreaView style={{ flex: 1 }}>
         <NavigationContainer>
-            <Drawer />
 
-            {/*
             <Tab />
+            {/*
+            <Drawer />
             <Stack />
             */}
         </NavigationContainer>
